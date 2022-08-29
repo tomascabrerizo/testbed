e0cbbe927f0fb4d2dd66e9480bd6a75b75144d5a
tomascabrerizo <tomascabrerizo97@gmail.com>
2022-08-29 19:54:48 -0300

    simple widget tree logic

--- frames.c
+++ frames.c
@@ -47,14 +47,103 @@ typedef enum FDokerType {
   F_DOKER_COUNT,
 } FDokerType;
 
+typedef unsigned int FHandle;
+#define FINVALID 0
+
 typedef struct FFrame {
   FRect rect;
   FRect clip;
   int border;
-  struct FFrame *doker[F_DOKER_COUNT];
+  FHandle doker[F_DOKER_COUNT];
+  FHandle parent;
+  FHandle handle;
+  void *id;
 } FFrame;
 
 typedef struct FState {
-  FFrame root;
+  FHandle root;
+  FHandle parent;
   FFrame *frame_buf;
+
 } FState;
+
+static FState state; /* TODO: for now frames state is global */
+void f_init() {
+  FFrame invalid = {0}; /* TODO: Invalid Handle 0 poinste to invalid frame */
+  invalid.handle = core_buf_size(state.frame_buf);
+  core_buf_push(state.frame_buf, invalid);
+  ASSERT(core_buf_size(state.frame_buf) > FINVALID);
+}
+
+void f_shutdown() {
+  core_buf_free(state.frame_buf);
+}
+
+void f_begin() {
+  state.root = FINVALID;
+  state.parent = FINVALID;
+}
+
+void f_end() {
+} 
+
+FFrame *f_frame(FHandle handle) {
+  ASSERT(handle > FINVALID);
+  ASSERT(handle < core_buf_size(state.frame_buf));
+  return &state.frame_buf[handle];
+}
+
+FFrame *f_get_frame_from_register(void *id) {
+  /* TODO: use a hash table instead of a streched buffer */
+  FFrame *buf = state.frame_buf;
+  for(unsigned int i = 0; i < (unsigned int)core_buf_size(buf); ++i) {
+    if(buf[i].id == id) {
+      return &buf[i];
+    }
+  }
+  return 0;
+}
+
+FHandle f_push_parent(void *id, FRect rect) {
+  FFrame *frame = f_get_frame_from_register(id);
+  if(!frame) {
+    FFrame f;
+    ASSERT(core_buf_size(state.frame_buf) > FINVALID);
+    f.handle = core_buf_size(state.frame_buf);
+    core_buf_push(state.frame_buf, f);
+    frame = f_frame(f.handle);
+
+  }
+  ASSERT(frame);
+  frame->rect = rect;
+  frame->clip = state.parent ? f_rect_intersection(frame->rect, f_frame(state.parent)->clip) : rect;
+  frame->border = 10;
+  frame->parent = state.parent;
+
+  if(!state.parent) {
+    state.root = frame->handle;
+  }
+  
+  state.parent = frame->handle;
+  
+  return frame->handle;
+}
+
+FHandle f_push_frame(void *id, FRect rect) {
+  FFrame *frame = f_get_frame_from_register(id);
+  if(!frame) {
+    FFrame f;
+    ASSERT(core_buf_size(state.frame_buf) > FINVALID);
+    f.handle = core_buf_size(state.frame_buf);
+    core_buf_push(state.frame_buf, f);
+    frame = f_frame(f.handle);
+  }
+  ASSERT(frame);
+  ASSERT(state.parent);
+  frame->rect = rect;
+  frame->clip = f_rect_intersection(frame->rect, f_frame(state.parent)->clip);
+  frame->border = 10;
+  frame->parent = state.parent;
+
+  return frame->handle;
+}
