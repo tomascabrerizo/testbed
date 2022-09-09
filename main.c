b34d661c47ea49e0b80d92ca3349e4b71c176a18
tomascabrerizo <tomascabrerizo97@gmail.com>
2022-09-09 16:32:26 -0300

    add horizontal split

--- main.c
+++ main.c
@@ -70,11 +70,8 @@ int main(void) {
     }
     
     render2d_begin(render);
-#if DOCK_TEST 
     frames_dock_test(window, render);
-#else
     frames_ui_test(ui);
-#endif
 
     render2d_end(render);
 
@@ -103,15 +100,26 @@ typedef struct VSplitter {
   struct RectContainer *l;
 } VSplitter;
 
+typedef struct HSplitter {
+  float position;
+  Rect dim;
+  struct RectContainer *t;
+  struct RectContainer *b;
+} HSplitter;
+
 typedef enum ContainerType {
   CONTAINER_RECT,
   CONTAINER_VSPLIT,
+  CONTAINER_HSPLIT,
 } ContainerType;
 
 typedef struct RectContainer {
   ContainerType type;
   Rect rect; 
-  VSplitter split;
+  union {
+    VSplitter vsplit;
+    HSplitter hsplit;
+  };
 } RectContainer;
 
 typedef struct Docker {
@@ -156,20 +164,31 @@ void draw_container(struct Render2D *render, RectContainer *container) {
       draw_rect(render, rect, rect_color);
     } break;
     case CONTAINER_VSPLIT: { 
-      draw_container(render, container->split.r); 
-      draw_container(render, container->split.l); 
-      draw_rect(render, container->split.dim, split_color);
+      draw_container(render, container->vsplit.r); 
+      draw_container(render, container->vsplit.l); 
+      draw_rect(render, container->vsplit.dim, split_color);
+    } break;
+    case CONTAINER_HSPLIT: { 
+      draw_container(render, container->vsplit.r); 
+      draw_container(render, container->vsplit.l); 
+      draw_rect(render, container->hsplit.dim, split_color);
     } break;
   }
 }
 
 void calculate_vsplit_size(RectContainer *container);
+void calculate_hsplit_size(RectContainer *container);
+
 void update_container(RectContainer *container) {
   ASSERT(container);
   if(container->type == CONTAINER_VSPLIT) { 
       calculate_vsplit_size(container);
-      update_container(container->split.l);
-      update_container(container->split.r);
+      update_container(container->vsplit.l);
+      update_container(container->vsplit.r);
+  } else if(container->type == CONTAINER_HSPLIT) { 
+      calculate_hsplit_size(container);
+      update_container(container->hsplit.t);
+      update_container(container->hsplit.b);
   }
 }
 
@@ -191,14 +210,29 @@ void dock_split_vertical(RectContainer *container) {
   ASSERT(container->type ==  CONTAINER_RECT);
   
   container->type = CONTAINER_VSPLIT;
-  container->split.position = 0.5f;
+  container->vsplit.position = 0.5f;
 
-  container->split.r = (RectContainer *)malloc(sizeof(RectContainer));
-  container->split.l = (RectContainer *)malloc(sizeof(RectContainer));
-  container->split.r->type = CONTAINER_RECT;
-  container->split.l->type = CONTAINER_RECT;
+  container->vsplit.r = (RectContainer *)malloc(sizeof(RectContainer));
+  container->vsplit.l = (RectContainer *)malloc(sizeof(RectContainer));
+  container->vsplit.r->type = CONTAINER_RECT;
+  container->vsplit.l->type = CONTAINER_RECT;
   
   //calculate_vsplit_size(container);
+}
+
+void dock_split_horizontal(RectContainer *container) {
+  ASSERT(container);
+  ASSERT(container->type ==  CONTAINER_RECT);
+  
+  container->type = CONTAINER_HSPLIT;
+  container->hsplit.position = 0.5f;
+
+  container->hsplit.t = (RectContainer *)malloc(sizeof(RectContainer));
+  container->hsplit.b = (RectContainer *)malloc(sizeof(RectContainer));
+  container->hsplit.t->type = CONTAINER_RECT;
+  container->hsplit.b->type = CONTAINER_RECT;
+  
+  //calculate_hsplit_size(container);
 }
 
 void calculate_vsplit_size(RectContainer *container) {
@@ -208,18 +242,39 @@ void calculate_vsplit_size(RectContainer *container) {
   /* NOTE: rects position */
   Rect r = rect;
   Rect l = rect;
-  l.r -= rect_dim * (1.0f - container->split.position);
-  r.l += rect_dim * container->split.position;
+  l.r -= rect_dim * (1.0f - container->vsplit.position);
+  r.l += rect_dim * container->vsplit.position;
   /* NOTE: split rect */
   int split_dim = 4;
   Rect split_rect = rect;
   /* TODO: Make f32_lerp function */
-  split_rect.l = rect.l*(1.0f - container->split.position) + rect.r*container->split.position - (split_dim/2);
+  split_rect.l = rect.l*(1.0f - container->vsplit.position) + rect.r*container->vsplit.position - (split_dim/2);
   split_rect.r = split_rect.l + split_dim;
 
-  container->split.r->rect = r;
-  container->split.l->rect = l;
-  container->split.dim = split_rect;
+  container->vsplit.r->rect = r;
+  container->vsplit.l->rect = l;
+  container->vsplit.dim = split_rect;
+}
+
+void calculate_hsplit_size(RectContainer *container) {
+  ASSERT(container->type == CONTAINER_HSPLIT); 
+  Rect rect = container->rect;
+  int rect_dim = (rect.b - rect.t);
+  /* NOTE: rects position */
+  Rect t = rect;
+  Rect b = rect;
+  b.t += rect_dim * container->hsplit.position;
+  t.b -= rect_dim * (1.0f - container->hsplit.position);
+  /* NOTE: split rect */
+  int split_dim = 4;
+  Rect split_rect = rect;
+  /* TODO: Make f32_lerp function */
+  split_rect.t = rect.t*(1.0f - container->hsplit.position) + rect.b*container->hsplit.position - (split_dim/2);
+  split_rect.b = split_rect.t + split_dim;
+
+  container->hsplit.b->rect = b;
+  container->hsplit.t->rect = t;
+  container->hsplit.dim = split_rect;
 }
 
 void frames_dock_test(struct CoreWindow *window, struct Render2D *render) {
@@ -227,9 +282,15 @@ void frames_dock_test(struct CoreWindow *window, struct Render2D *render) {
   if(!init) {
     dock_init(window);
     dock_split_vertical(&docker.root);
-    docker.root.split.position = 0.4f;
-    dock_split_vertical(docker.root.split.r);
-    docker.root.split.r->split.position = 0.7f;
+    docker.root.vsplit.position = 0.4f;
+    dock_split_vertical(docker.root.vsplit.r);
+    docker.root.vsplit.r->vsplit.position = 0.7f;
+
+    dock_split_horizontal(docker.root.vsplit.l);
+    docker.root.vsplit.l->hsplit.position = 0.6f;
+
+    dock_split_horizontal(docker.root.vsplit.r->vsplit.l);
+
     init = true;
   }
   docker_update(window);
